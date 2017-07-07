e519269ff63b51062ccbfc5765d0443ea2a2299f
Josh <joshpitzalis@gmail.com>
2017-07-07 11:23:06 +0530

    you can now add tasks

---
+++ frontend/cypress/integration/test_spec.js
@@ -0,0 +1,8 @@
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    cy.visit('https://joshpitzalis.com')
+    expect(true).to.equal(true)
+    cy.get('a').contains('See More Work').click()
+    cy.url().should('include', '/now')
+  })
+})
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Deliverable from './Deliverables.js'
 import Add from './Add'
+import { firebaseAuth, ref } from '../constants/firebase.js'
 
 export default class Dashboard extends Component {
   state = {
@@ -16,17 +17,24 @@ export default class Dashboard extends Component {
         ready: true
       }
     ],
-    ThingsYouNeedToDo: [
-      {
-        title: 'stuffs',
-        from: 'Someone',
-        due: 'tomorrow',
-        link: 'http://localhost:3000/dashboard',
-        ready: false
-      }
-    ],
-    add: true
+    thingsYouNeedToDo: [],
+    add: false
   }
+
+  componentDidMount() {
+    this.getTasksINeedToDo()
+  }
+
+  toggleAddModal = () => {
+    this.setState({ add: !this.state.add })
+  }
+
+  getTasksINeedToDo = () => {
+    ref
+      .child(`/users/${firebaseAuth().currentUser.uid}/tasks`)
+      .on('value', snap => this.setState({ thingsYouNeedToDo: snap.val() }))
+  }
+
   render() {
     const ThingsYouWillGet = this.state.ThingsYouWillGet.map((item, index) =>
       <Deliverable
@@ -38,18 +46,21 @@ export default class Dashboard extends Component {
       />
     )
 
-    const ThingsYouNeedToDo = this.state.ThingsYouNeedToDo.map((item, index) =>
+    const ThingsYouNeedToDo = Object.keys(
+      this.state.thingsYouNeedToDo
+    ).map((item, index) =>
       <Deliverable
         key={index}
-        ready={item.ready}
-        title={item.title}
-        from={item.from}
-        due={item.due}
+        ready={false}
+        myStuff={true}
+        title={this.state.thingsYouNeedToDo[item].deliverable}
+        from={this.state.thingsYouNeedToDo[item].client}
+        due={this.state.thingsYouNeedToDo[item].deadline}
       />
     )
     return (
       <div className="mw6 center tc">
-        {this.state.add && <Add/>}
+        {this.state.add && <Add closeAddModal={this.toggleAddModal} />}
         <p className="f4 tc mv3 b">PEOPLE</p>
         <div className="pv3">
           {ThingsYouWillGet}
@@ -58,7 +69,10 @@ export default class Dashboard extends Component {
         <div className="pv3">
           {ThingsYouNeedToDo}
         </div>
-        <button className="link dim dib f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60 mv5" onClick={()=> console.log('dog')}>
+        <button
+          className="link dim dib f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60 mv5"
+          onClick={this.toggleAddModal}
+        >
           Add a New Thing
         </button>
       </div>
