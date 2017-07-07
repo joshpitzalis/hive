8513edbcab3d2cfc40f77d5e9e47f37e4afbf4bd
Josh <joshpitzalis@gmail.com>
2017-07-07 13:43:09 +0530

    added a cypress test

---
+++ cypress/integration/tests_spec.js
@@ -0,0 +1,9 @@
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    // cy.visit('https://joshpitzalis.com')
+    // expect(true).to.equal(true)
+    // cy.get('a').contains('See More Work').click()
+    // cy.url().should('include', '/now')
+    expect(true).to.equal(true)
+  })
+})
--- src/tests.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-it('tests that unit tests work', () => {
-  expect(true).toBe(true)
-})
