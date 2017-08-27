e8caa3ccc0310472e4cb569cccb6bb6aa9d404ba
Josh <joshpitzalis@gmail.com>
2017-08-27 12:22:52 +0800

    not suer what teh client branch is for but stashing changes just incase

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { createNewTask } from '../helpers/database.js'
 import Close from '../styles/images/close.js'
+import { Checkbox, Label } from 'rebass'
+import { CardElement } from 'react-stripe-elements'
 
 export default class Add extends Component {
   static propTypes = {}
@@ -9,7 +11,12 @@ export default class Add extends Component {
   state = {
     deliverable: null,
     client: null,
-    deadline: null
+    deadline: null,
+    paid: false,
+    number: null,
+    cvv: null,
+    month: null,
+    year: null
   }
 
   handleChange = fieldName => evt => {
@@ -32,7 +39,7 @@ export default class Add extends Component {
     return (
       <div className="flex fixed top-0 left-0 h-100 w-100 bg-black-60 z-1">
         <div className="flex mxc cxc w-100 h-100">
-          <div className="bg-white pa3 w5 w-40-ns">
+          <div className="bg-white pa3 w5 w-40-ns h-75 overflow-y-auto">
             <div className="tr dim pointer" onClick={this.props.closeAddModal}>
               <Close />
             </div>
@@ -61,14 +68,75 @@ export default class Add extends Component {
               value={this.state.deadline}
             />
 
+            <Label className="mt4 pa2 w-80 center">
+              <Checkbox
+                type="checkbox"
+                name="paid"
+                value={this.state.paid}
+                onChange={() => this.setState({ paid: !this.state.paid })}
+              />
+              or I'll be charged $5 everyday that I don't.
+            </Label>
+            {this.state.paid && <Checkout />}
+            <label>
+              Card details
+              <CardElement style={{ base: { fontSize: '18px' } }} />
+            </label>
             <input
               type="submit"
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 db ma4 center"
               onClick={this.handleSubmit}
             />
+            <div id="card-errors" role="alert" />
           </div>
         </div>
       </div>
     )
   }
 }
+
+const Checkout = ({ props }) =>
+  <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+    <legend className="ph0 mh0 fw6 clip">Add Your Payment Details</legend>
+    <div className="mt3">
+      <label className="db fw4 lh-copy f6" for="email-address">
+        Card Number
+      </label>
+      <input
+        className="pa2 input-reset ba bg-transparent w-80 measure"
+        type="number"
+        name="email-address"
+        id="email-address"
+      />
+    </div>
+    <div className="mt3">
+      <label className="db fw4 lh-copy f6" for="password">
+        CVV
+      </label>
+      <input
+        className="b pa2 input-reset ba bg-transparent"
+        type="number"
+        name="password"
+        id="password"
+      />
+    </div>
+    <div className="mt3">
+      <label className="db fw4 lh-copy f6" for="Expiry">
+        Expiry Date
+      </label>
+      <div className="tc">
+        <input
+          className="b pa2 input-reset ba bg-transparent w3"
+          type="number"
+          name="Expiry"
+          id="month"
+        />
+        <input
+          className="b pa2 input-reset ba bg-transparent w4"
+          type="number"
+          name="Expiry"
+          id="year"
+        />
+      </div>
+    </div>
+  </fieldset>
--- src/helpers/database.js
+++ src/helpers/database.js
@@ -31,3 +31,40 @@ export function uploadDeliverable(
     .update({ file, url, timeDelivered, ready: true })
     .catch(error => console.error(error))
 }
+
+// export function submitNewCreditCard(
+//   number,
+//   cvv,
+//   exp_month,
+//   exp_year,
+//   address_zip
+// ) {
+//   Stripe.card.createToken(
+//     {
+//       number,
+//       cvc,
+//       exp_month,
+//       exp_year,
+//       address_zip
+//     },
+//     (status, response) => {
+//       if (response.error) {
+//         this.newCreditCard.error = response.error.message
+//       } else {
+//         firebase
+//           .database()
+//           .ref(`/stripe_customers/${this.currentUser.uid}/sources`)
+//           .push({ token: response.id })
+//           .then(() => {
+//             this.newCreditCard = {
+//               number: '',
+//               cvc: '',
+//               exp_month: 1,
+//               exp_year: 2017,
+//               address_zip: ''
+//             }
+//           })
+//       }
+//     }
+//   )
+// }
