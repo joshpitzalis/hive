3e83034424f756f58264fffa0f9b3f27cff31954
Josh <joshpitzalis@gmail.com>
2017-09-02 12:41:42 +0800

    encrypted secret keys on travis

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import App from './components/routes.js'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/styles.css'
 import { StripeProvider } from 'react-stripe-elements'
-import stripeKey from './constants/stripe.js'
+// import stripeKey from './constants/stripe.js'
 
 ReactDOM.render(
-  <StripeProvider apiKey={stripeKey}>
+  <StripeProvider apiKey={'pk_test_UEtlVFtmDH1SIsFR5HMOR6tD'}>
     <App />
   </StripeProvider>,
   document.getElementById('root')
